2d7b49cb66dfcf72470105f71c064a78257f8dc4
Luis Leiva <lleiva@sciling.com>
2019-12-11 14:55:42 +0200

    Updated keyb implementation, added dummy keys

--- js/keyboard-impl.js
+++ js/keyboard-impl.js
@@ -1,121 +1,118 @@
 (function() {
 
   function getKeyboards(self) {
-    // Shorthand methods.
+    // Shorthand methods. See default settings below.
     var w = self.settings.keyWidth
       , h = self.settings.keyHeight
       , l = self.settings.keyHorizontalMargin || self.settings.keyMargin
       , t = self.settings.keyVerticalMargin || self.settings.keyMargin
       ;
 
-    function kx(row, col, offset) {
+    function x(row, col, offset) {
       return w * col + l + (offset || 0);
     }
 
-    function ky(row, col, offset) {
+    function y(row, col, offset) {
       return h * row + t + (offset || 0);
     }
 
     return [
+      // Traditional QWERTY layout.
+      { 'name': "abc",
+        'keys': [
+        [ x(0, 0),         y(0),  w,  h, "q" ],
+        [ x(0, 1),         y(0),  w,  h, "w" ],
+        [ x(0, 2),         y(0),  w,  h, "e" ],
+        [ x(0, 3),         y(0),  w,  h, "r" ],
+        [ x(0, 4),         y(0),  w,  h, "t" ],
+        [ x(0, 5),         y(0),  w,  h, "y" ],
+        [ x(0, 6),         y(0),  w,  h, "u" ],
+        [ x(0, 7),         y(0),  w,  h, "i" ],
+        [ x(0, 8),         y(0),  w,  h, "o" ],
+        [ x(0, 9),         y(0),  w,  h, "p" ],
 
-      { 'name': "abc"
-      , 'keys': [
-        kx(0, 0),  ky(0),  w,  h, "q",
-        kx(0, 1),  ky(0),  w,  h, "w",
-        kx(0, 2),  ky(0),  w,  h, "e",
-        kx(0, 3),  ky(0),  w,  h, "r",
-        kx(0, 4),  ky(0),  w,  h, "t",
-        kx(0, 5),  ky(0),  w,  h, "y",
-        kx(0, 6),  ky(0),  w,  h, "u",
-        kx(0, 7),  ky(0),  w,  h, "i",
-        kx(0, 8),  ky(0),  w,  h, "o",
-        kx(0, 9),  ky(0),  w,  h, "p",
+        [ x(1, 0, w/2),    y(1),  w,  h, "a" ],
+        [ x(1, 1, w/2),    y(1),  w,  h, "s" ],
+        [ x(1, 2, w/2),    y(1),  w,  h, "d" ],
+        [ x(1, 3, w/2),    y(1),  w,  h, "f" ],
+        [ x(1, 4, w/2),    y(1),  w,  h, "g" ],
+        [ x(1, 5, w/2),    y(1),  w,  h, "h" ],
+        [ x(1, 6, w/2),    y(1),  w,  h, "j" ],
+        [ x(1, 7, w/2),    y(1),  w,  h, "k" ],
+        [ x(1, 8, w/2),    y(1),  w,  h, "l" ],
 
-        kx(1, 0, w/2),  ky(1),  w,  h, "a",
-        kx(1, 1, w/2),  ky(1),  w,  h, "s",
-        kx(1, 2, w/2),  ky(1),  w,  h, "d",
-        kx(1, 3, w/2),  ky(1),  w,  h, "f",
-        kx(1, 4, w/2),  ky(1),  w,  h, "g",
-        kx(1, 5, w/2),  ky(1),  w,  h, "h",
-        kx(1, 6, w/2),  ky(1),  w,  h, "j",
-        kx(1, 7, w/2),  ky(1),  w,  h, "k",
-        kx(1, 8, w/2),  ky(1),  w,  h, "l",
+        [ x(2, 0, w*1.5),  y(2),  w,  h, "z" ],
+        [ x(2, 1, w*1.5),  y(2),  w,  h, "x" ],
+        [ x(2, 2, w*1.5),  y(2),  w,  h, "c" ],
+        [ x(2, 3, w*1.5),  y(2),  w,  h, "v" ],
+        [ x(2, 4, w*1.5),  y(2),  w,  h, "b" ],
+        [ x(2, 5, w*1.5),  y(2),  w,  h, "n" ],
+        [ x(2, 6, w*1.5),  y(2),  w,  h, "m" ],
 
-//        kx(1, 0, w/2),    ky(2),  w,  h, "\u21E7",
-        kx(2, 0, w*1.5),  ky(2),  w,  h, "z",
-        kx(2, 1, w*1.5),  ky(2),  w,  h, "x",
-        kx(2, 2, w*1.5),  ky(2),  w,  h, "c",
-        kx(2, 3, w*1.5),  ky(2),  w,  h, "v",
-        kx(2, 4, w*1.5),  ky(2),  w,  h, "b",
-        kx(2, 5, w*1.5),  ky(2),  w,  h, "n",
-        kx(2, 6, w*1.5),  ky(2),  w,  h, "m",
-//        kx(2, 7, w*1.5),  ky(2),  w,  h, "\u21D0",
+        [ x(3, 0, w/2),    y(3),  w*2,  h*0.7, "", true],
+        [ x(3, 1, w*1.5),  y(3),  w,    h*0.7, "", true],
+        [ x(3, 2, w*1.5),  y(3),  w*4,  h*0.7, "", true], // <-- true means "dummy" key
+        [ x(3, 6, w*1.5),  y(3),  w,    h*0.7, "", true],
+        [ x(3, 7, w*1.5),  y(3),  w,    h*0.7, "", true],
 
-//        kx(3, 1, w*1.5), ky(3), w*5, h, " ",
+      ]},
 
-//        kx(2, 7, w),  ky(2),  w,  h, ",",
-//        kx(2, 8, w),  ky(2),  w,  h, ".",
-//        kx(2, 7, w),  ky(2),  w+w/2,  h, "\u21D0",
+      // Numerical layout.
+      { 'name': "#1",
+        'keys': [
+        [ x(0, 0),  y(0),  w,  h, "1" ],
+        [ x(0, 1),  y(0),  w,  h, "2" ],
+        [ x(0, 2),  y(0),  w,  h, "3" ],
+        [ x(0, 3),  y(0),  w,  h, "4" ],
+        [ x(0, 4),  y(0),  w,  h, "5" ],
+        [ x(0, 5),  y(0),  w,  h, "6" ],
+        [ x(0, 6),  y(0),  w,  h, "7" ],
+        [ x(0, 7),  y(0),  w,  h, "8" ],
+        [ x(0, 8),  y(0),  w,  h, "9" ],
+        [ x(0, 9),  y(0),  w,  h, "0" ],
 
-//        kx(3, 0, w*2), ky(3), w, h, "←",
-//        kx(3, 1, w*2), ky(3), w*5, h, " ",
-//        kx(3, 6, w*2), ky(3), w, h, "→",
-      ]}
-    ,
-      { 'name': "#1"
-      , 'keys': [
-        kx(0, 0),  ky(0),  w,  h, "1",
-        kx(0, 1),  ky(0),  w,  h, "2",
-        kx(0, 2),  ky(0),  w,  h, "3",
-        kx(0, 3),  ky(0),  w,  h, "4",
-        kx(0, 4),  ky(0),  w,  h, "5",
-        kx(0, 5),  ky(0),  w,  h, "6",
-        kx(0, 6),  ky(0),  w,  h, "7",
-        kx(0, 7),  ky(0),  w,  h, "8",
-        kx(0, 8),  ky(0),  w,  h, "9",
-        kx(0, 9),  ky(0),  w,  h, "0",
+        [ x(1, 0, w/2),  y(1),  w,  h, "!",  ],
+        [ x(1, 1, w/2),  y(1),  w,  h, "\"" ],
+        [ x(1, 2, w/2),  y(1),  w,  h, "·" ],
+        [ x(1, 3, w/2),  y(1),  w,  h, "$" ],
+        [ x(1, 4, w/2),  y(1),  w,  h, "%" ],
+        [ x(1, 5, w/2),  y(1),  w,  h, "&" ],
+        [ x(1, 6, w/2),  y(1),  w,  h, "/" ],
+        [ x(1, 7, w/2),  y(1),  w,  h, "(" ],
+        [ x(1, 8, w/2),  y(1),  w,  h, ")" ],
 
-        kx(1, 0, w/2),  ky(1),  w,  h, "!",
-        kx(1, 1, w/2),  ky(1),  w,  h, "\"",
-        kx(1, 2, w/2),  ky(1),  w,  h, "·",
-        kx(1, 3, w/2),  ky(1),  w,  h, "$",
-        kx(1, 4, w/2),  ky(1),  w,  h, "%",
-        kx(1, 5, w/2),  ky(1),  w,  h, "&",
-        kx(1, 6, w/2),  ky(1),  w,  h, "/",
-        kx(1, 7, w/2),  ky(1),  w,  h, "(",
-        kx(1, 8, w/2),  ky(1),  w,  h, ")",
+//        [ x(2, 0, 3*w/2), y(2), w, h, "←" ],
+//        [ x(2, 1, 3*w/2), y(2), w*5, h, " " ],
+//        [ x(2, 6, 3*w/2), y(2), w, h, "→" ],
+      ]},
 
-//        kx(2, 0, 3*w/2), ky(2), w, h, "←",
-        kx(2, 1, 3*w/2), ky(2), w*5, h, " ",
-//        kx(2, 6, 3*w/2), ky(2), w, h, "→",
-      ]}
-    ,
-      { 'name': "#2"
-      , 'keys': [
-        kx(0, 0),  ky(0),  w,  h, "'",
-        kx(0, 1),  ky(0),  w,  h, '"',
-        kx(0, 2),  ky(0),  w,  h, ":",
-        kx(0, 3),  ky(0),  w,  h, ";",
-        kx(0, 4),  ky(0),  w,  h, "-",
-        kx(0, 5),  ky(0),  w,  h, "+",
-        kx(0, 6),  ky(0),  w,  h, "*",
-        kx(0, 7),  ky(0),  w,  h, "?",
-        kx(0, 8),  ky(0),  w,  h, "^",
-        kx(0, 9),  ky(0),  w,  h, "ç",
+      // Numerical layout.
+      { 'name': "#2",
+        'keys': [
+        [ x(0, 0),  y(0),  w,  h, "'" ],
+        [ x(0, 1),  y(0),  w,  h, '"' ],
+        [ x(0, 2),  y(0),  w,  h, ":" ],
+        [ x(0, 3),  y(0),  w,  h, ";" ],
+        [ x(0, 4),  y(0),  w,  h, "-" ],
+        [ x(0, 5),  y(0),  w,  h, "+" ],
+        [ x(0, 6),  y(0),  w,  h, "*" ],
+        [ x(0, 7),  y(0),  w,  h, "?" ],
+        [ x(0, 8),  y(0),  w,  h, "^" ],
+        [ x(0, 9),  y(0),  w,  h, "ç" ],
 
-        kx(1, 0, w/2),  ky(1),  w,  h, "`",
-        kx(1, 1, w/2),  ky(1),  w,  h, "~",
-        kx(1, 2, w/2),  ky(1),  w,  h, "<",
-        kx(1, 3, w/2),  ky(1),  w,  h, ">",
-        kx(1, 4, w/2),  ky(1),  w,  h, "{",
-        kx(1, 5, w/2),  ky(1),  w,  h, "}",
-        kx(1, 6, w/2),  ky(1),  w,  h, "[",
-        kx(1, 7, w/2),  ky(1),  w,  h, "]",
-        kx(1, 8, w/2),  ky(1),  w,  h, "\\",
+        [ x(1, 0, w/2),  y(1),  w,  h, "`" ],
+        [ x(1, 1, w/2),  y(1),  w,  h, "~" ],
+        [ x(1, 2, w/2),  y(1),  w,  h, "<" ],
+        [ x(1, 3, w/2),  y(1),  w,  h, ">" ],
+        [ x(1, 4, w/2),  y(1),  w,  h, "{" ],
+        [ x(1, 5, w/2),  y(1),  w,  h, "}" ],
+        [ x(1, 6, w/2),  y(1),  w,  h, "[" ],
+        [ x(1, 7, w/2),  y(1),  w,  h, "]" ],
+        [ x(1, 8, w/2),  y(1),  w,  h, "\\" ],
 
-//        kx(2, 0, 3*w/2), ky(2), w, h, "←",
-        kx(2, 1, 3*w/2), ky(2), w*5, h, " ",
-//        kx(2, 6, 3*w/2), ky(2), w, h, "→",
+//        [ x(2, 0, 3*w/2), y(2), w, h, "←" ],
+//        [ x(2, 1, 3*w/2), y(2), w*5, h, " " ],
+//        [ x(2, 6, 3*w/2), y(2), w, h, "→" ],
       ]}
 
     ];
@@ -124,6 +121,7 @@
   // Expose module
   var keyboard = {
 
+    // Deault keyboard settings. Globally available.
     settings: {
         offsetTop: 0
       , offsetLeft: 0
@@ -132,31 +130,33 @@
       , keyMargin: 0
       , keyVerticalMargin: 0
       , keyHorizontalMargin: 0
-    }
+    },
 
-//  , getKeyboards: function() {
+//  getKeyboards: function() {
 //      return getKeyboards(this);
-//  }
+//  },
 
-  , getLayout: function(index) {
+    getLayout: function(index) {
       var keyboards = getKeyboards(this);
       var keyb = [];
-      var keys = keyboards[index].keys;
+      var keys = keyboards[index || 0].keys;
       var hMargin = this.settings.keyHorizontalMargin || this.settings.keyMargin,
           vMargin = this.settings.keyVerticalMargin || this.settings.keyMargin;
-      for (var i = 0; i < keys.length; i += 5) {
+      for (var i = 0; i < keys.length; i++) {
+        var row = keys[i];
         keyb.push({
-            x:      keys[i]   + this.settings.offsetLeft
-          , y:      keys[i+1] + this.settings.offsetTop
-          , width:  keys[i+2] - hMargin
-          , height: keys[i+3] - vMargin
-          , char:   keys[i+4]
+            x:      row[0]   + this.settings.offsetLeft
+          , y:      row[1] + this.settings.offsetTop
+          , width:  row[2] - hMargin
+          , height: row[3] - vMargin
+          , char:   row[4]
+          , dummy:  row[5]
         });
       }
       return keyb;
-    }
+    },
 
-  , measure: function(index) {
+    measure: function(index) {
       var keys = this.getLayout(index);
       var hSize = 0,
           vSize = 0;
